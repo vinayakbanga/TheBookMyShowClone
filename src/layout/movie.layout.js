2cef47e314eb3c390d65b0efae15f52a07810815
Vinayak Banga <vinayakbanga22@gmail.com>
2022-01-28 22:24:41 +0530

    Links to Movie page added

--- src/layout/movie.layout.js
+++ src/layout/movie.layout.js
@@ -9,7 +9,7 @@ import MovieNavbar from "../components/Navbar/movieNavbar.component"
 
 import { MovieCOntext } from "../context/movie.context";
 import Links from "../components/Linking/linking.component";
-
+import { Link } from "react-router-dom";
 
 const MovieLayout = (props)=>{
 
@@ -33,6 +33,18 @@ const MovieLayout = (props)=>{
         <>
        <MovieNavbar/>
        <Links/>
+       <header class="text-white body-font bg-gray-900">
+        <div class="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
+         
+          <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 flex flex-wrap items-center text-base justify-center">
+          <Link to={`/`}> <a class="mr-5 hover:text-gray-400 text-xl">Home</a>  </Link>
+          
+          <Link to={`/plays`}> <a class="mr-5 hover:text-gray-400 text-xl">Plays</a> </Link>
+          
+          </nav>
+         
+        </div>
+      </header>
        
        {props.children};
        </>
